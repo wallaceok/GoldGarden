b5edc2f8a1966ab4836dba1e93b75c0df2338353
xiaoxiaolulu <546464268@qq.com>
2017-11-12 16:38:52 +0800

    add result report

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 # coding:utf-8
 import unittest
 import time
+import os
 from public import HTMLTestRunner
 from config import global_parameters
 
@@ -13,9 +14,13 @@ def run(rule="test*.py"):
         pattern=rule,
         top_level_dir=None
     )
+    day = time.strftime('%Y-%m-%d')
+    report_path = global_parameters.report_ptah+'\\'+day
+    if not os.path.isdir(report_path):
+        os.makedirs(report_path)
     now = time.strftime('%Y-%m-%d_%H_%M_%S')
-    report_name = global_parameters.report_ptah+'\\'+now+'_TestReport'+'.html'
-    with open(report_name, 'wb') as fb:
+    test_report_name = report_path +'\\'+now+'_TestReport.html'
+    with open(test_report_name, 'wb') as fb:
         runner = HTMLTestRunner.HTMLTestRunner(
             stream=fb,
             title='Jyb_Test_Report',
