e250c70c176b0e0ed17c26ef7a2e44a849e54602
xiaoxiaolulu <546464268@qq.com>
2017-11-12 16:28:36 +0800

    add result report

--- run.py
+++ run.py
@@ -1,19 +1,30 @@
 #!/usr/bin/python3
 # coding:utf-8
 import unittest
+import time
+from public import HTMLTestRunner
 from config import global_parameters
 
 
-def add_test(rule="test*.py"):
+def run(rule="test*.py"):
     case_path = global_parameters.case_path
-    discover = unittest.defaultTestLoader.discover(case_path, pattern=rule, top_level_dir=None)
-    run = unittest.TextTestRunner()
-    run.run(discover)
+    discover = unittest.defaultTestLoader.discover(
+        case_path,
+        pattern=rule,
+        top_level_dir=None
+    )
+    now = time.strftime('%Y-%m-%d_%H_%M_%S')
+    report_name = global_parameters.report_ptah+'\\'+now+'_TestReport'+'.html'
+    with open(report_name, 'wb') as fb:
+        runner = HTMLTestRunner.HTMLTestRunner(
+            stream=fb,
+            title='Jyb_Test_Report',
+            description='用例测试情况'
+        )
+        runner.run(discover)
 
 if __name__ == '__main__':
-    import fmovice
-    print(fmovice.Search_Movice('QTP视频'))
-    # add_test()
+    run()
 
 
 
